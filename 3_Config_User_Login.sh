d087b6bbd79009bed114a85836b578ce9c0bcfea
Codey <amanthind@live.de>
2017-06-29 12:54:52 +0200

    Don't throw error if a dir exists

--- 3_Config_User_Login.sh
+++ 3_Config_User_Login.sh
@@ -25,7 +25,7 @@ DEBIAN_FRONTEND=noninteractive pam-auth-update
 
 
 # Add a field for a user name, disable user selection at the login screen
-mkdir /etc/lightdm/lightdm.conf.d
+mkdir -p /etc/lightdm/lightdm.conf.d
 cat >>/etc/lightdm/lightdm.conf.d/99-show-manual-userlogin.conf <<__EOF__
 [SeatDefaults]
 greeter-show-manual-login=true
